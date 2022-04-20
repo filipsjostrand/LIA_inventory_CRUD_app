b434de457bb2db214b23425048627c32e0e69fe7
filipsjostrand <92868216+filipsjostrand@users.noreply.github.com>
2022-04-20 18:00:13 +0200

    First commit

---
+++ src/main/java/nxrgrp/lia_inventory_crud_app/LiaInventoryCrudAppApplication.java
@@ -0,0 +1,13 @@
+package nxrgrp.lia_inventory_crud_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiaInventoryCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiaInventoryCrudAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nxrgrp/lia_inventory_crud_app/LiaInventoryCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nxrgrp.lia_inventory_crud_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiaInventoryCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
