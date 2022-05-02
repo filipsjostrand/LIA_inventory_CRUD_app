89e28670400445d2f284a9d20651c1f0e195b0e3
filipsjostrand <92868216+filipsjostrand@users.noreply.github.com>
2022-05-02 10:22:27 +0200

    Added more comments to project branch

--- src/main/java/nxrgrp/lia_inventory_crud_app/LiaInventoryCrudAppApplication.java
+++ src/main/java/nxrgrp/lia_inventory_crud_app/LiaInventoryCrudAppApplication.java
@@ -3,6 +3,8 @@ package nxrgrp.lia_inventory_crud_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// React JS + Spring Boot REST API Example Tutorial https://www.youtube.com/watch?v=5RA5NpxbioI&ab_channel=JavaGuides
+
 @SpringBootApplication
 public class LiaInventoryCrudAppApplication {
 
