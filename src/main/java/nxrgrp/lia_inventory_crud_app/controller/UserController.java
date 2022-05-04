47b91defe634a80ca2331230aa33c9a44b121bfa
filipsjostrand <92868216+filipsjostrand@users.noreply.github.com>
2022-05-04 11:10:57 +0200

    fix crossorigin

--- src/main/java/nxrgrp/lia_inventory_crud_app/controller/UserController.java
+++ src/main/java/nxrgrp/lia_inventory_crud_app/controller/UserController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import nxrgrp.lia_inventory_crud_app.model.User;
 import nxrgrp.lia_inventory_crud_app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // Create Rest-API with simple rest-end-point "users"
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/")
 public class UserController {
